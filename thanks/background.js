11d22be561fe974454123ecb87038dfb9e587718
Christopher Palileo <cpalileo@gmail.com>
2022-09-14 06:58:45 -0700

    installation thank you page created

--- thanks/background.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.onInstalled.addListener(async () => {
-  let url = chrome.runtime.getUrl("welcome/hello.html");
-  let tab = await chrome.tabs.create({});
-});
---
+++ thanks/script.js
@@ -0,0 +1,6 @@
+//opens the thank you page into a new tab at runtime
+chrome.runtime.onInstalled.addListener(async () => {
+  let url = chrome.runtime.getURL("thanks/index.html");
+  let tab = await chrome.tabs.create({ url });
+  console.log(`Created tab ${tab.id}`);
+});
