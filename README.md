93e198de3d77fce78b2d718586cbaee08fbc5078
Christopher Palileo <cpalileo@gmail.com>
2022-09-16 16:52:18 -0700

    source control

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # auto-attendance
 
-# Chrome Extension to that takes attendance for online school with the click of a button.
+## Chrome Extension to that takes attendance for online school with the click of a button.
 
-# Icon Credit
+## Current Features: Auto fill, Save & Load buttons functioning.
+
+## Features In Progress: Refactor to auto save & load credentials.
+
+### Icon Credit:
 
 <a href="https://www.flaticon.com/free-icons/verification" title="verification icons">Verification icons created by juicy_fish - Flaticon</a>
--- ext/index.js
+++ ext/index.js
@@ -1,30 +1,36 @@
 document.getElementById("btn-start").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {
-      email: document.getElementById("email").value,
-      pass: document.getElementById("pass").value,
+  autoLoad(),
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        email: document.getElementById("email").value,
+        pass: document.getElementById("pass").value,
+      });
+    }),
+    autoSave();
+});
+
+function autoSave() {
+  document.getElementById("btn-save").addEventListener("click", () => {
+    chrome.storage.sync.set(
+      {
+        email: document.getElementById("email").value,
+        pass: document.getElementById("pass").value,
+      },
+      function () {
+        console.log("Email and Password Saved " + email + " " + pass);
+      }
+    );
+  });
+}
+
+function autoLoad() {
+  document.getElementById("btn-load").addEventListener("click", () => {
+    chrome.storage.sync.get(["email", "pass"], function (result) {
+      (document.getElementById("email").value = result.email),
+        (document.getElementById("pass").value = result.pass);
     });
   });
-});
-
-document.getElementById("btn-save").addEventListener("click", () => {
-  chrome.storage.sync.set(
-    {
-      email: document.getElementById("email").value,
-      pass: document.getElementById("pass").value,
-    },
-    function () {
-      console.log("Email and Password Saved " + email + " " + pass);
-    }
-  );
-});
-
-document.getElementById("btn-load").addEventListener("click", () => {
-  chrome.storage.sync.get(["email", "pass"], function (result) {
-    (document.getElementById("email").value = result.email),
-      (document.getElementById("pass").value = result.pass);
-  });
-});
+}
 // document.getElementsByClassName("btn-primary")
 
 //Refactoring needed.  Have login credentials auto save and auto load with click of one button
