dd4fdc8ed07746686d7c3da6e7dbe30bab13d0bf
Christopher Palileo <cpalileo@gmail.com>
2022-09-16 13:18:13 -0700

    source control

--- ext/index.js
+++ ext/index.js
@@ -1,29 +1,25 @@
-document.getElementById("btn-login").addEventListener("click", () => {
-  // console.log("Checkpoint");
+document.getElementById("btn-start").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(
       tabs[0].id,
       {
-        email: document.getElementById("user-email").value,
-        pass: document.getElementById("user-pass").value,
+        email: document.getElementById("email").value,
+        pass: document.getElementById("pass").value,
       },
       function (response) {
         console.log(response.status);
       }
     );
   });
+  chrome.storage.local.set(
+    {
+      email: email.value,
+      pass: pass.value,
+    },
+    function () {
+      console.log("Email and Password Saved " + email + " " + pass);
+    }
+  );
 });
-
-// document.getElementById("btn-save").addEventListener(click, () => {
-//   chrome.storage.sync.set(
-//     {
-//       email: getElementById("email").value,
-//       pass: getElementById("pass").value,
-//     },
-//     function () {
-//       console.log("Saved");
-//     }
-//   );
-// });
 
 //Refactoring needed.  Have login credentials auto save and auto load with click of one button
