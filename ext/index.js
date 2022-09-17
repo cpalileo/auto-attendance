270e72135db68713ab87cca57264fb6bbe3c5743
Christopher Palileo <cpalileo@gmail.com>
2022-09-17 10:31:09 -0700

    minor scrub

--- ext/index.js
+++ ext/index.js
@@ -1,12 +1,10 @@
 document.getElementById("btn-start").addEventListener("click", () => {
-  autoLoad(),
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        email: document.getElementById("email").value,
-        pass: document.getElementById("pass").value,
-      });
-    }),
-    autoSave();
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      email: document.getElementById("email").value,
+      pass: document.getElementById("pass").value,
+    });
+  });
 });
 
 function autoSave() {
