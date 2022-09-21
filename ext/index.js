bd532e5a9fa79051acb3398a2a048afc96e32160
Christopher Palileo <cpalileo@gmail.com>
2022-09-20 21:38:37 -0700

    autoFill, autoSave, and autoLoad working, attempting autoClick

--- ext/index.js
+++ ext/index.js
@@ -1,3 +1,4 @@
+// Auto fill login credentials function
 const autoFill = () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {
@@ -7,6 +8,7 @@ const autoFill = () => {
   });
 };
 
+// Auto save login credentials function
 const autoSave = () => {
   chrome.storage.sync.set({
     email: document.getElementById("email").value,
@@ -14,6 +16,16 @@ const autoSave = () => {
   });
 };
 
+// Function to auto click login button
+
+//Attempt
+// const loginDiv = document.getElementsByClassName(".form-validate");
+// const clickLogin = loginDiv.getElementsByTagName("button");
+
+//Attempt
+const submitButton = document.getElementsByTagName("button")[0];
+
+// Function to auto load last used login credentials
 function autoLoad() {
   chrome.storage.sync.get(["email", "pass"], function (result) {
     (document.getElementById("email").value = result.email),
@@ -21,13 +33,18 @@ function autoLoad() {
   });
 }
 
+// Upon load envoke function to autoload credentials
 autoLoad();
 
+// Event listener for extension button
 document.getElementById("btn-start").addEventListener("click", () => {
   autoFill();
   autoSave();
+  submitButton.click(),
+    function (response) {
+      console.log(response.status);
+    };
 });
-
 // document.getElementsByClassName("btn-primary")
 
 //Refactoring needed.  Have login credentials auto save and auto load with click of one button
